cdf7e45e15f34d54206a1ee85c4d62a4fa14a023
Onyedika Christopher <cosimon.tech@gmail.com>
2026-08-27 01:46:13 +0100

    feat: add main script to run parcel retrieval

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from courier.storage import load_parcels
 from courier.index import build_index
+from courier.services import get_parcel
 
 
 parcels = load_parcels()
 tracking_index = build_index(parcels)
 
-print("Parcels loaded:", len(parcels))
-print("Index enteries:", len(tracking_index))
+# print("Parcels loaded:", len(parcels))
+# print("Index enteries:", len(tracking_index))
 
-print(tracking_index["SA-1998500-IY"])
+# print(tracking_index["SA-1998500-IY"])
+
+code = "SA-1998500-IY"
+
+status, result = get_parcel(
+    code,
+    parcels,
+    tracking_index
+)
+
+print(status)
+print(result)
